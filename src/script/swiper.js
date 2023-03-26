dac349a548341674a7f1e857da146dc12056bc2c
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-03-26 12:51:52 +0300

    save

--- src/script/swiper.js
+++ src/script/swiper.js
@@ -2,11 +2,24 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+  breakpoints: {
+    // when window width is >= 320px
+    375: {
+      slidesPerView: 1,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 640px
+    1200: {
+      slidesPerView: 4,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
   },
 
   // Navigation arrows
