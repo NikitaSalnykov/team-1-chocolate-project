162d8f0fd75293e11e5834bbc9d9abecb8548408
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-03-30 01:40:56 +0300

    RECOVERY VERSION v1.0

--- src/script/main.js
+++ src/script/main.js
@@ -1,10 +1,10 @@
-// import './modal-leave';
-// import './thankyou';
-// import './modals';
-// import './modal';
-// import './mobile-menu';
-// import './modal-for-mobile-menu';
-// import './swiper-our-prod';
-// import './swiper';
-// import './swipers';
+import './mobile-menu';
+import './modal-for-mobile-menu';
+import './modal-leave';
+import './thankyou';
+import './modals';
+import './modal';
+import './swiper-our-prod';
+import './swiper';
+import './swipers';
 
