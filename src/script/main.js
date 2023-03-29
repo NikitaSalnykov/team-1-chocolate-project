34a623f88160188ab0f3fe8ed3b5be42123c4cb1
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-03-30 00:40:53 +0300

    ss

--- src/script/main.js
+++ src/script/main.js
@@ -1,11 +1,10 @@
-import './swiper-our-prod';
-import './swiper';
-import './mobile-menu';
-import './modal-for-mobile-menu';
-import './swipers';
 import './modal-leave';
 import './thankyou';
-// import './modals';
+import './modals';
 import './modal';
-const modals = require('./modals');
+import './mobile-menu';
+import './modal-for-mobile-menu';
+import './swiper-our-prod';
+import './swiper';
+import './swipers';
 
