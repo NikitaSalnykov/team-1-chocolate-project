ca8c0390e15333ca21458765532c1c88fb2002f9
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-03-30 02:20:04 +0300

    Revert "RECOVERY VERSION v1.0"
    This reverts commit 162d8f0fd75293e11e5834bbc9d9abecb8548408.

---
+++ src/script/main.js
@@ -0,0 +1,10 @@
+// import './modal-leave';
+// import './thankyou';
+// import './modals';
+// import './modal';
+// import './mobile-menu';
+// import './modal-for-mobile-menu';
+// import './swiper-our-prod';
+// import './swiper';
+// import './swipers';
+
