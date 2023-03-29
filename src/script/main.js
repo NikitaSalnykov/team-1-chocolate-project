71ae80c67f68302fd334ec186d40a7d6dcaf3b1d
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-03-30 00:27:31 +0300

    Update main.js

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,10 @@
 import './swiper-our-prod';
 import './swiper';
-import './modal';
 import './mobile-menu';
 import './modal-for-mobile-menu';
 import './swipers';
 import './modal-leave';
 import './thankyou';
 import './modals';
+import './modal';
+
