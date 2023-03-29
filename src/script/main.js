5c443388b95d39291087df6587b4d033e997c821
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-03-30 02:21:59 +0300

    Revert "pls"
    This reverts commit 5961e756c24c0aefe2507030ff33e425dcbaccc2.

---
+++ src/script/main.js
@@ -0,0 +1,10 @@
+import './mobile-menu';
+import './modal-for-mobile-menu';
+import './modal-leave';
+import './thankyou';
+import './modals';
+import './modal';
+import './swiper-our-prod';
+import './swiper';
+import './swipers';
+
