ff64ee6be11c2af75625e9f63b637a413d2e3a6d
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-03-29 13:19:14 +0300

    ssssssaaaaaaavvvvvveeeee

--- src/script/thankyou.js
+++ src/script/thankyou.js
@@ -2,10 +2,24 @@ const toggleBtn = document.getElementById("toggle-btn-send");
 const modal = document.getElementById("modal-thankyou");
 const lastmodal = document.getElementById("modal-leave");
 const closeBtn = modal.querySelector(".close-button-thank-you");
+// заполни поля Name + Email + phone
+const nameInput = document.getElementById("name-leave");
+const emailInput = document.getElementById("email-leave");
+const phoneInput = document.getElementById("tel-leave"); 
 
-toggleBtn.addEventListener("click", () => {
+toggleBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  if (!nameInput.value || nameInput.checkValidity()) { 
+    alert("Name is empty");
+  } else if (!emailInput.value || emailInput.checkValidity()) {
+    alert("Email is empty");
+  } else if (!phoneInput.value) {
+    alert("Phone is empty");
+  } else {
     lastmodal.style.display = "none";
     modal.style.display = "block";
+  }  
 });
 
 closeBtn.addEventListener("click", () => {
@@ -13,7 +27,7 @@ closeBtn.addEventListener("click", () => {
 });
 
 window.addEventListener("click", (event) => {
-  if (event.target === modal) {
+  if (event.target === modal ) {
     modal.style.display = "none";
   }
 });
