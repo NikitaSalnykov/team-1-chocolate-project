ee60dd1e728b26f3021c42a39372ae7e120ebae4
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-03-30 00:48:46 +0300

    d

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,9 @@
 // import './modal-leave';
 // import './thankyou';
 import './modals';
-import './modal';
-// import './mobile-menu';
-// import './modal-for-mobile-menu';
+// import './modal';
+import './mobile-menu';
+import './modal-for-mobile-menu';
 // import './swiper-our-prod';
 // import './swiper';
 // import './swipers';
