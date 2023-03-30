9b493fe67fb4050090573c58218050737c44ac8f
tarastupko <109355475+tarastupko@users.noreply.github.com>
2023-03-30 03:55:44 +0300

    ggg

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,7 @@
-console.log("Hi world.");
+import './swiper-our-prod';
+import './swiper';
+//import './modal';
+import './mobile-menu';
+import './modal-for-mobile-menu';
+import './swipers';
+import './modals';
